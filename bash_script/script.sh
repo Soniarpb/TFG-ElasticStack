9a770850f234df864baa9f46b8067836953fb094
Soniarpb <47895159+Soniarpb@users.noreply.github.com>
2022-03-09 21:45:58 +0100

    Update script.sh

--- bash_script/script.sh
+++ bash_script/script.sh
@@ -2,7 +2,7 @@
 url=https://covid.ourworldindata.org/data/owid-covid-data.csv
 d=$(date +%Y-%m-%d)
 
-wget -O covid-data-$d --directory-prefix=/root/TFG/docker-elk-main/logstash/pipeline/data/COVID/ $url
+wget -O covid-data-$d --directory-prefix=/root/TFG-ElasticStack/docker-elk-main/logstash/pipeline/data/COVID/ $url
 
-mv /root/TFG/docker-elk-main/logstash/pipeline/data/COVID/covid-data-$d /root/TFG/docker-elk-main/logstash/pipeline/data/COVID/covid-data-$d.csv
+mv /root/TFG-ElasticStack/docker-elk-main/logstash/pipeline/data/COVID/covid-data-$d /root/TFG-ElasticStack/docker-elk-main/logstash/pipeline/data/COVID/covid-data-$d.csv
 
